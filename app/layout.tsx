6f6ceb112c760004b267e418b1efabecbd79f963
Thaira <thairachoudhury@gmail.com>
2026-09-06 00:10:42 -0400

    delete unused components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "@/components/old components/navbar"; 
 
 
 export const metadata: Metadata = {
--- components/old components/smallStar.tsx
+++
@@ -1,9 +0,0 @@
-export default function SmallStar() {
-    return (
-
-    <svg  viewBox="0 0 249 343" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full h-auto">
-        <path d="M154.265 -4.70706e-05L144.14 140.234L248.201 104.758L152.424 176.065L218.036 275.829L132.384 206.902L93.9359 342.142L104.061 201.908L5.4562e-05 237.384L95.7766 166.077L30.1645 66.3127L115.816 135.24L154.265 -4.70706e-05Z" fill="#808249"/>
-    </svg>
-
-    );
-}
